7755e998cfbcd09e3cd4f50293ad42539edf427b
macotok <monochromer@r8.ucom.ne.jp>
2020-01-12 01:57:46 +0900

    firebaseでhosting

--- my-app-ts/src/index.js
+++ my-app-ts/src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import firebase from 'firebase';
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
