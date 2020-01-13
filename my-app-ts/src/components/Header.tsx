5d76d96f50fe9a6ee259302c0e7ab18b0597149c
macotok <monochromer@r8.ucom.ne.jp>
2020-01-13 23:58:52 +0900

    headerにサイトの説明文を追加

--- my-app-ts/src/components/Header.tsx
+++ my-app-ts/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useContext } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Alert, Nav, Navbar } from 'react-bootstrap';
 import { Link } from '@reach/router';
 import AppContext from '../contexts/AppContext';
 import { IEpisode, IState } from '../interfaces';
@@ -7,13 +7,23 @@ import { IEpisode, IState } from '../interfaces';
 const Header: FC = () => {
   const { state: { favorites } }: { state: IState, favorites: IEpisode[] } = useContext(AppContext);
   return (
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand>Check Favorite Episodes</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Item style={{marginRight: "1rem"}}><Link to="/">Home</Link></Nav.Item>
-        <Nav.Item><Link to="/favorite">favorite{favorites.length > 1 ? 's' : null}: {favorites.length}</Link></Nav.Item>
-      </Nav>
-    </Navbar>
+    <>
+      <Alert variant="secondary">
+        このサイトのRepositoryは「
+        <a href="https://github.com/macotok/udemy-Typescript-with-React" target="_blank">
+          macotok/udemy-Typescript-with-React
+        </a>
+        」<br />
+        ReactにTypeScriptを導入しました。
+      </Alert>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand>Check Favorite Episodes</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Item style={{marginRight: "1rem"}}><Link to="/">Home</Link></Nav.Item>
+          <Nav.Item><Link to="/favorite">favorite{favorites.length > 1 ? 's' : null}: {favorites.length}</Link></Nav.Item>
+        </Nav>
+      </Navbar>
+    </>
   );
 };
 
